cc4d6693f98302cbea80c7548ea55ecf297a61ef
lionX-s <7883557+lionxs@user.noreply.gitee.com>
2020-12-15 23:12:36 +0800

    feat(master):跨组件通信Context的使用

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import ReactDOM from 'react-dom';
 // import App from './3.组件的数据传递/4.父传子通信-属性验证'
 // import App from './3.组件的数据传递/5.子传父通信-函数传递'
 // import App from './4.组件通信案例/App'
-import App from './5.react实现slot/App'
+// import App from './5.react实现slot/App'
+import App from './6.跨组件通信/1.跨组件通信'
 
 
 ReactDOM.render(
