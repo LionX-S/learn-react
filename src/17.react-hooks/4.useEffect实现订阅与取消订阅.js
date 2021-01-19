45ee1452a8f07bb9e4622cba89996e6b5211639b
LionX-S <13141096848@163.com>
2021-01-19 21:49:20 +0800

    feat(master):useEffect的基本使用以及依赖

---
+++ src/17.react-hooks/4.useEffect实现订阅与取消订阅.js
@@ -0,0 +1,18 @@
+import React,{useEffect}from 'react'
+
+export default function App() {
+  // 传入第二个参数[],会进行优化，如果不传入，则当前组件重新渲染一次，useEffect则也会重新执行一次
+  // 第二个参数为数组，元素为依赖的数据，只有依赖的数据更新时才会重新执行。
+  useEffect(() => {
+    console.log('订阅消息');
+    // 返回一个函数，则会在组件卸载时执行,
+    return () => {
+      console.log('取消订阅');
+    }
+  },[]);
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/17.react-hooks/5.多个useEffect的使用.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from 'react'
+
+export default function App() {
+  // 多个useEffect按照顺序执行
+  useEffect(() => {
+    console.log('修改DOM');
+  });
+  useEffect(() => {
+    console.log('订阅事件');
+  });
+  useEffect(() => {
+    console.log('网络请求');
+  });
+  return (
+    <div>
+      
+    </div>
+  )
+}
